c02405a45e941f5291a2e58768405630dcc263e3
AbrarNoob <hasinabrarhaque@gmail.com>
2023-10-21 02:01:31 +0600

    sm8250-common: Sync with tree changes

--- sm8250-common-vendor.mk
+++ sm8250-common-vendor.mk
@@ -782,4 +782,5 @@ PRODUCT_PACKAGES += \
     android.hardware.neuralnetworks@1.3-service-qti \
     manifest_android.hardware.drm@1.3-service.widevine \
     manifest_vendor.xiaomi.hardware.mlipay \
+    manifest_vendor.dolby.hardware.dms \
     vendor.qti.gnss@4.0-service
